4954c84017b9e5bb2845439fe1bcfc304d0b5663
boomernag <william.dahlberg.n@gmail.com>
2020-12-08 02:26:17 +0000

    added models

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,16 @@
+import uuid
+
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+class TimeSlotBooking(models.Model):
+    customer_email = models.CharField(max_length=100)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    start_timestamp = models.TimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.start_timestamp
--- timeslots/models.py
+++ timeslots/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 # Create your models here.
-    
+
 class TimeSlot(models.Model):
+    timeslot_id = models.CharField(max_length=20)
     number_of_possible_bookings = models.PositiveIntegerField()
-    is_slot_available = models.CharField(max_length=1,choices=CHOICES)
-    is_slot_full = models.CharField(max_length=1, choices=CHOICES)
+    is_slot_available = models.CharField(max_length=1)
+    is_slot_full = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.timeslot_id
